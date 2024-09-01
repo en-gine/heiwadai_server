bb4f48bfbdd839067e05393946c9325640d79208
sutefu23 <sutefu23@gmail.com>
2024-09-01 11:58:09 +0900

    CRONにログ追加

--- server/router/AuthCronSecret.go
+++ server/router/AuthCronSecret.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 
 	"server/infrastructure/env"
+	"server/infrastructure/logger"
 
 	"github.com/bufbuild/connect-go"
 )
@@ -17,14 +18,19 @@ func NewAuthCronHeader() connect.Option {
 			// リクエストヘッダーからTokenを取得する
 			cronSecret := req.Header().Get("Authorization")
 			cronKey := req.Header().Get("X-Cron-Key")
+			logger.DebugPrint(req.Header())
+
 			if cronSecret == "" || cronKey == "" {
+				logger.Error("認証ヘッダーが必要なアクセスです。")
 				return nil, connect.NewError(connect.CodeUnauthenticated, errors.New("認証ヘッダーが必要なアクセスです。"))
 			}
 
 			if cronSecret != env.GetEnv(env.CronAccessSecret) {
+				logger.Error("認証情報（シークレットキー）が正しくありません。")
 				return nil, connect.NewError(connect.CodeUnauthenticated, errors.New("認証情報（シークレットキー）が正しくありません。"))
 			}
 			if cronKey != env.GetEnv(env.CronAccessKey) {
+				logger.Error("認証情報（キー情報）が正しくありません。")
 				return nil, connect.NewError(connect.CodeUnauthenticated, errors.New("認証情報（キー情報）が正しくありません。"))
 			}
 
