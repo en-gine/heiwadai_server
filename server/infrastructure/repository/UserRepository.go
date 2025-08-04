91c90a9db7f215a41fc2bd1e1cb732fc2bc23ca8
sutefu23 <sutefu23@gmail.com>
2025-08-04 16:39:04 +0900

    レポジトリエラー修正

--- server/infrastructure/repository/UserRepository.go
+++ server/infrastructure/repository/UserRepository.go
@@ -162,3 +162,15 @@ func (ur *UserRepository) Delete(userID uuid.UUID) error {
 
 	return nil
 }
+
+func (ur *UserRepository) DeleteUnderRegisterUser(userID uuid.UUID) error {
+	_, err := ur.db.Exec(fmt.Sprintf("DELETE FROM auth.users WHERE id = '%s'", userID.String()))
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil
+		}
+		logger.Error(err.Error())
+		return err
+	}
+	return nil
+}
